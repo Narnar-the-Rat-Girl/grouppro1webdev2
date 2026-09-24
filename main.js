87246e116f2e2ca63a54c2e8eef7f554d7bdd680
Ashley <61482604+Narnar-the-Rat-Girl@users.noreply.github.com>
2026-09-24 14:41:34 -0600

    .

--- main.js
+++ main.js
@@ -6,17 +6,27 @@ const weightResult = document.getElementById('weight-result');
 
 
 const kilogramsToPounds = (kilograms) => kilograms * 2.20462;
-const handleKgConvert = () => {
-    const kilograms = Number(kgInput.value);
-    const pounds = kilogramsToPounds(kilograms);
-    weightResult.textContent = pounds.toFixed(2);
+const KgConvert = () => {
+    let text = "";
+    const kilograms = kgInput.value.split(',');
+    for(k of kilograms) {
+        k = Number(k);
+        const pounds = kilogramsToPounds(k);
+        text += pounds.toFixed(2) + " lbs, ";
+
+        
+    }    
+    weightResult.textContent = text;
 };
-kgButton.addEventListener("click", handleKgConvert);
+poundsButton.addEventListener("click", KgConvert);
 
 const poundsToKilograms = (pounds) => pounds / 2.20462;
-const handlePoundsConvert = () => {
+const PoundsConvert = () => {
+
+
+    
     const pounds = Number(poundsInput.value);
     const kilograms = poundsToKilograms(pounds);
-    weightResult.textContent = kilograms.toFixed(2);
+    weightResult.textContent = kilograms.toFixed(2) + " kg";
 };
-poundsButton.addEventListener("click", handlePoundsConvert);
+kgButton.addEventListener("click", PoundsConvert);
