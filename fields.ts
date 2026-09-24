65cb56aad27376be6e576d4fc1a456dbc3da70ba
Camille Tsui <camsthingz@gmail.com>
2026-09-24 12:25:47 -0600

    added distance.html

---
+++ fields.ts
@@ -0,0 +1,10 @@
+
+//not done btw
+const wrapper = document.getElementById('fields-wrapper') as HTMLDivElement | null;
+const addButton = document.getElementById('add-field-btn') as HTMLElement | null;
+
+if (wrapper && addButton) {
+    addButton.addEventListener('click', (e:MouseEvent): void => {
+
+    })
+}
